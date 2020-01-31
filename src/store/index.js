ec91b8e017412b9180ae2cd22a40dedbcb12c04c
fspereiraDev <filipe-pereira95@hotmail.com>
2020-01-31 17:30:35 +0000

    CSS changes

--- src/store/index.js
+++ src/store/index.js
@@ -49,15 +49,15 @@ export default new Vuex.Store({
       tags: ["Sports", "Literature", "Music", "Nature"],
       user_type: 2
     }
-    ],
-    locations: [{
+  ],
+  locations: [{
       id: 1,
       image: "https://www.comerciocomhistoria.gov.pt/wp-content/uploads/import/listings/3351_imagem2.jpg",
       name: "Livraria Lello",
       lat: "41.1469055",
       long: "-8.6169633",
       address: "R. das Carmelitas 144, 4050-161 Porto",
-      description: "A Livraria Lello é uma porta viva de acesso à história: pela preservação do edifício, pela manutenção da sua função original como livraria, pelo papel de embaixadora da cultura e pela dinâmica cultural que continua a imprimir na cidade.",
+      description: "Livraria Lello is a living gateway to history: for the preservation of the building, for the maintenance of its original function as a bookstore, for the role of ambassador of culture and for the cultural dynamics that continues to print in the city.",
       rating: "",
       website: "https://www.livrarialello.pt/pt-pt/",
       tag: "Literature"
@@ -69,7 +69,7 @@ export default new Vuex.Store({
       lat: "",
       long: "",
       address: "Via Futebol Clube do Porto, 4350-415 Porto",
-      description: "O Estádio do Dragão é um estádio de futebol, localizado na freguesia de Campanhã, cidade do Porto, atualmente propriedade do FC Porto, sendo neste recinto que a equipa de futebol joga as suas partidas em casa.",
+      description: "Estádio do Dragão is a football stadium, located in the parish of Campanhã, city of Porto, currently owned by FC Porto, where the football team plays its matches at home.",
       rating: "",
       website: "https://www.fcporto.pt/pt/clube/estadio-do-dragao",
       tag: "Sports"
@@ -81,7 +81,7 @@ export default new Vuex.Store({
       lat: "41.146992",
       long: "-8.6076057",
       address: "R. de Passos Manuel 137, 4000-385 Porto",
-      description: "O Coliseu do Porto é uma sala de espectáculos localizada na cidade do Porto, em Portugal. O edifício foi classificado como Monumento de Interesse Público pela Portaria n.º 637/2012, de 2 de novembro de 2012, publicada em Diário da República.",
+      description: "Coliseu do Porto is a concert hall located in the city of Porto, in Portugal. The building was classified as a Monument of Public Interest by Ordinance No. 637/2012, of November 2, 2012, published in Diário da República.",
       rating: "",
       website: "https://www.coliseu.pt/",
       tag: "Music"
@@ -93,14 +93,14 @@ export default new Vuex.Store({
       lat: "41.146992",
       long: "-8.6076057",
       address: "R. Dom João de Castro 210, 4150-417 Porto",
-      description: "O Coliseu do Porto é uma sala de espectáculos localizada na cidade do Porto, em Portugal. O edifício foi classificado como Monumento de Interesse Público pela Portaria n.º 637/2012, de 2 de novembro de 2012, publicada em Diário da República.",
+      description: "The Serralves Foundation is located in the Serralves Park in the city of Porto, in Portugal, where the Serralves House and the Serralves Contemporary Art Museum are located.",
       rating: "",
       website: "https://www.serralves.pt/pt/",
       tag: "Nature"
     },
-    ],
-    currentLocation: {},
-    reviews: [{
+  ],
+  currentLocation: {},
+  reviews: [{
       id: 1,
       nameUser: "Rokas Grabauskas",
       image: "https://s3.amazonaws.com/gazelle.cdn.yolocare.com/sites/361/2018/01/04135946/yolo-icons-160x160-nursemale2.png",
@@ -116,66 +116,48 @@ export default new Vuex.Store({
       texto: "One of the biggest stadiums I've ever visited! A lot of great european nights in that picth!",
       rate: 5
     }
-    ],
-    itineraries: [{
+  ],
+  itineraries: [{
       id: 1,
       title: "Stadium Tour",
       image: "https://huddersfieldspanishtuitions.files.wordpress.com/2016/04/img_2857.jpg?w=665",
       locations: [{
 
-        id: 1,
-<<<<<<< HEAD
-=======
-        image: "https://www.comerciocomhistoria.gov.pt/wp-content/uploads/import/listings/3351_imagem2.jpg",
-        name: "Livraria Lello",
-        lat: "41.1469055",
-        long: "-8.6169633",
-        address: "R. das Carmelitas 144, 4050-161 Porto",
-        description: "Livraria Lello is a living gateway to history: for the preservation of the building, for the maintenance of its original function as a bookstore, for the role of ambassador of culture and for the cultural dynamics that continues to print in the city.",
-        rating: "",
-        website: "https://www.livrarialello.pt/pt-pt/",
-        tag: "Literature"
-      },
-      {
-        id: 2,
-        image: "https://files.app.fcporto.pt/sources/5c90c108c4b3b1J17qAEFdZDq1XiD.jpg",
->>>>>>> c886848dc182476de601ef7f898b10d46f6c6a5b
-        name: "Estádio do Dragão",
-        description: "The Estádio do Dragão is an all-seater football stadium located in Porto, Portugal. It is the current home stadium of FC Porto with a capacity of 50,033, making it the third largest football stadium in Portugal. Designed by Portuguese architect Manuel Salgado, the infrastructure was constructed to replace Porto's previous stadium, the Estádio das Antas, along with becoming one of the host venues for the UEFA Euro 2004 tournament finals. The inauguration took place on 16 November 2003 with a friendly match against Barcelona, setting an attendance record of 52,000 spectators.",
-        city: "Porto",
-        address: "Via Futebol Clube do Porto, 4350-415 Porto",
-<<<<<<< HEAD
-<<<<<<< HEAD
-        image: "",
-        rating: 0,
-        lat: 41.16177,
-        long: -8.5857797,
-        tag: "desporto"
-      },
-      {
-        id: 2,
-        name: "Estádio do Bessa Séc. XXI",
-        description: "A soccer stadium of portugal",
-        city: "Porto",
-        address: "Estádio do Bessa Seculo XXI, Rua O Primeiro deJaneiro, 4100-365 Porto",
-        image: "",
-        rating: 0,
-        lat: 41.1624892,
-        long: -8.6454498,
-        tag: "desporto"
-      },
-      {
-        id: 3,
-        name: "Estádio dos Arcos",
-        description: "A soccer stadium of portugal located in Vila do Conde",
-        city: "Vila do Conde",
-        address: "R. Dom Sancho I, 4480-876 Vila do Conde",
-        image: "",
-        rating: 0,
-        lat: 41.3627211,
-        long: -8.7422818,
-        tag: "desporto"
-      }
+          id: 1,
+          name: "Estádio do Dragão",
+          description: "The Estádio do Dragão is an all-seater football stadium located in Porto, Portugal. It is the current home stadium of FC Porto with a capacity of 50,033, making it the third largest football stadium in Portugal. Designed by Portuguese architect Manuel Salgado, the infrastructure was constructed to replace Porto's previous stadium, the Estádio das Antas, along with becoming one of the host venues for the UEFA Euro 2004 tournament finals. The inauguration took place on 16 November 2003 with a friendly match against Barcelona, setting an attendance record of 52,000 spectators.",
+          city: "Porto",
+          address: "Via Futebol Clube do Porto, 4350-415 Porto",
+          image: "",
+          rating: 0,
+          lat: 41.16177,
+          long: -8.5857797,
+          tag: "desporto"
+        },
+        {
+          id: 2,
+          name: "Estádio do Bessa Séc. XXI",
+          description: "A soccer stadium of portugal",
+          city: "Porto",
+          address: "Estádio do Bessa Seculo XXI, Rua O Primeiro deJaneiro, 4100-365 Porto",
+          image: "",
+          rating: 0,
+          lat: 41.1624892,
+          long: -8.6454498,
+          tag: "desporto"
+        },
+        {
+          id: 3,
+          name: "Estádio dos Arcos",
+          description: "A soccer stadium of portugal located in Vila do Conde",
+          city: "Vila do Conde",
+          address: "R. Dom Sancho I, 4480-876 Vila do Conde",
+          image: "",
+          rating: 0,
+          lat: 41.3627211,
+          long: -8.7422818,
+          tag: "desporto"
+        }
       ]
     },
     {
@@ -183,82 +165,46 @@ export default new Vuex.Store({
       title: "Museum Tour",
       image: "https://i.pinimg.com/originals/02/fe/cb/02fecb6d98822d12103b4bfa9fcf56aa.jpg",
       locations: [{
-=======
-=======
->>>>>>> c886848dc182476de601ef7f898b10d46f6c6a5b
-        description: "Estádio do Dragão is a football stadium, located in the parish of Campanhã, city of Porto, currently owned by FC Porto, where the football team plays its matches at home.",
-        rating: "",
-        website: "https://www.fcporto.pt/pt/clube/estadio-do-dragao",
-        tag: "Sports"
-      },
-      {
-        id: 3,
-        image: "https://www.medis.pt/media/1998/coliseu-porto-agora-e-coliseu-porto-ageas.jpg?anchor=center&mode=crop&width=1440&height=895&rnd=131719894510000000",
-        name: "Coliseu Porto Ageas",
-        lat: "41.146992",
-        long: "-8.6076057",
-        address: "R. de Passos Manuel 137, 4000-385 Porto",
-        description: "Coliseu do Porto is a concert hall located in the city of Porto, in Portugal. The building was classified as a Monument of Public Interest by Ordinance No. 637/2012, of November 2, 2012, published in Diário da República.",
-        rating: "",
-        website: "https://www.coliseu.pt/",
-        tag: "Music"
-      },
-      {
-        id: 4,
-        image: "https://www.cp.pt/StaticFiles/Passageiros/4_vantagens/vantagens/cultura/serralves-g.jpg",
-        name: "Serralves",
-        lat: "41.146992",
-        long: "-8.6076057",
-        address: "R. Dom João de Castro 210, 4150-417 Porto",
-        description: "The Serralves Foundation is located in the Serralves Park in the city of Porto, in Portugal, where the Serralves House and the Serralves Contemporary Art Museum are located.",
-        rating: "",
-        website: "https://www.serralves.pt/pt/",
-        tag: "Nature"
-      },
-    ],
-    currentLocation: {},
-    reviews: [{
->>>>>>> c886848dc182476de601ef7f898b10d46f6c6a5b
-        id: 1,
-        name: "Wolrd of Discoveries",
-        description: "The FC Porto Museum, officially known as FC Porto Museum by BMG (Portuguese: Museu do Futebol Clube do Porto by BMG) for sponsorship reasons, is a museum located in Porto, dedicated to the history of the Portuguese association football club FC Porto. It was inaugurated on 28 September 2013, on occasion of the club's 120th anniversary, and opened its doors to the general public on 26 October.",
-        city: "Porto",
-        address: "Rua de Miragaia 106, 4050-387 Porto",
-        image: "",
-        rating: 0,
-        lat: 41.160769,
-        long: -8.5849047,
-        tag: "museums"
-      },
-      {
-        id: 2,
-        name: "Misericórdia do Porto Museum",
-        description: "World of Discoveries is an Interactive Museum and Theme Park dedicated to Portuguese Discoveries located inside the former warehouses of Real Companhia Velha, in Miragaia, in the historic center of Porto.",
-        city: "Porto",
-        address: "R. das Flores 15, 4050-265 Porto",
-        image: "",
-        rating: 0,
-        lat: 41.160769,
-        long: -8.5849047,
-        tag: "museums"
-      },
-      {
-        id: 3,
-        name: "Serralves Museum",
-        description: "The Museum of Contemporary Art of the Serralves Foundation is in the Portuguese city of Porto. The building, designed by the architect Siza Vieira, is surrounded by the Serralves Park.",
-        city: "Porto",
-        address: "R. Dom João de Castro 210, 4150-417 Porto",
-        image: "",
-        rating: 0,
-        lat: 41.160769,
-        long: -8.5849047,
-        tag: "museums"
-      }
+          id: 1,
+          name: "Wolrd of Discoveries",
+          description: "The FC Porto Museum, officially known as FC Porto Museum by BMG (Portuguese: Museu do Futebol Clube do Porto by BMG) for sponsorship reasons, is a museum located in Porto, dedicated to the history of the Portuguese association football club FC Porto. It was inaugurated on 28 September 2013, on occasion of the club's 120th anniversary, and opened its doors to the general public on 26 October.",
+          city: "Porto",
+          address: "Rua de Miragaia 106, 4050-387 Porto",
+          image: "",
+          rating: 0,
+          lat: 41.160769,
+          long: -8.5849047,
+          tag: "museums"
+        },
+        {
+          id: 2,
+          name: "Misericórdia do Porto Museum",
+          description: "World of Discoveries is an Interactive Museum and Theme Park dedicated to Portuguese Discoveries located inside the former warehouses of Real Companhia Velha, in Miragaia, in the historic center of Porto.",
+          city: "Porto",
+          address: "R. das Flores 15, 4050-265 Porto",
+          image: "",
+          rating: 0,
+          lat: 41.160769,
+          long: -8.5849047,
+          tag: "museums"
+        },
+        {
+          id: 3,
+          name: "Serralves Museum",
+          description: "The Museum of Contemporary Art of the Serralves Foundation is in the Portuguese city of Porto. The building, designed by the architect Siza Vieira, is surrounded by the Serralves Park.",
+          city: "Porto",
+          address: "R. Dom João de Castro 210, 4150-417 Porto",
+          image: "",
+          rating: 0,
+          lat: 41.160769,
+          long: -8.5849047,
+          tag: "museums"
+        }
       ]
     }
-    ],
-    currentItinerary: {},
-    locationVisitors: []
+  ],
+  currentItinerary: {},
+  locationVisitors: []
 
   },
   getters: {
